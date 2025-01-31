3d47febf9d7e009fd7b1410e94d70285da740230
Ilham Hakim <ilhmhakim@gmail.com>
2025-01-31 14:02:26 +0700

    feat: Category

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,11 @@
+import { format, parseISO } from 'date-fns';
+import { id } from 'date-fns/locale';
+
+export const formatDateTime = (isoString: string) => {
+  const date = parseISO(isoString);
+  return format(date, 'HH:mm, eeee dd MMMM yyyy', { locale: id });
+};
+
 export const truncateText = (value: string, limit: number) => {
   if (value.length <= limit) {
     return value;
