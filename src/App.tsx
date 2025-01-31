fbb4adb8a975147d805ac22392a1b124f99a5428
Adi Satria <adisatria@apps.ipb.ac.id>
2025-01-31 11:10:13 +0700

    feat: integrate Refine for resource management and add CreateSeries route

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import theme from "./theme";
+// import Landing from "./pages/Landing";
+
 import { Refine } from "@refinedev/core";
+import { useNotificationProvider } from "@refinedev/mui";
 import dataProvider from "@refinedev/simple-rest";
+
 import { ErrorBoundary } from "react-error-boundary";
 import AdminLayout from "./components/AdminLayout";
 import { ErrorFallback } from "./error/ErrorFallback";
@@ -30,17 +34,6 @@ import RoleManager from "./pages/RoleManager";
 import { LoadingProvider } from "./providers/LoadingProvider";
 import { SnackbarProvider } from "./providers/SnackbarProvider";
 import CreateSeries from "./pages/master-data/CreateSeries";
-import {Category} from "@/pages/master-data/Category.tsx";
-import {GroupTest} from "@/pages/master-data/group-test/GroupTest.tsx";
-import GroupTestCreateEdit from "@/pages/master-data/group-test/GroupTestCreateEdit.tsx";
-import SubTest from "@/pages/master-data/sub-test/SubTest.tsx";
-import {Test} from "@/pages/master-data/test/Test.tsx";
-import TestCreateEdit from "@/pages/master-data/test/TestCreateEdit.tsx";
-import SubTestCreateEdit from "@/pages/master-data/sub-test/SubTestCreateEdit.tsx";
-import SeriesDetails from "@/pages/master-data/SeriesDetails.tsx";
-import {Batch} from "@/pages/master-data/batch/Batch.tsx";
-import BatchCreateEdit from "@/pages/master-data/batch/BatchCreateEdit.tsx";
-import {EmailTemplate} from "@/pages/master-data/EmailTemplate.tsx";
 
 const WelcomeClient = lazy(() => import("@/pages/WelcomeClient"));
 const RouteProtector = lazy(() => import("@/protector/RouteProtector"));
@@ -111,10 +104,6 @@ const router = createBrowserRouter([
         element: <Series />,
       },
       {
-        path: "series/:id",
-        element: <SeriesDetails/>
-      },
-      {
         path: "series/create",
         element: <CreateSeries />,
       },
@@ -166,58 +155,6 @@ const router = createBrowserRouter([
         path: "role/edit/:id",
         element: <CreateEditRole />,
       },
-      {
-        path: "category",
-        element: <Category />,
-      },
-      {
-        path: "subtest",
-        element: <SubTest/>
-      },
-      {
-        path: "subtest/create",
-        element: <SubTestCreateEdit/>
-      },
-      {
-        path: "subtest/edit/:id",
-        element: <SubTestCreateEdit/>
-      },
-      {
-        path: "test",
-        element: <Test/>
-      },
-      {
-        path: "test/create",
-        element: <TestCreateEdit/>
-      },
-      {
-        path: "test/edit/:id",
-        element: <TestCreateEdit/>
-      },
-      {
-        path: "grouptest",
-        element: <GroupTest/>
-      },
-      {
-        path: "grouptest/create",
-        element: <GroupTestCreateEdit/>
-      },
-      {
-        path: "grouptest/edit/:id",
-        element: <GroupTestCreateEdit/>
-      },
-      {
-        path: "batch",
-        element: <Batch/>
-      },
-      {
-        path: "email-template",
-        element: <EmailTemplate/>
-      }
-      // {
-      //   path: "batch/create",
-      //   element: <BatchCreateEdit/>
-      // }
     ],
   },
 ]);
