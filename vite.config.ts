8f08fff50c9e8ca800ee437a91b2bd5ca779d52b
Adi Satria <adisatria@apps.ipb.ac.id>
2025-01-31 11:10:30 +0700

    feat: add CreateSeries component for series creation functionality

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -16,5 +17,3 @@ export default defineConfig({
     emptyOutDir: true,
   },
 });
-
-
