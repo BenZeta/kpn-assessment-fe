5b0b9cf4c7f1bbca0950ce0dd2257bcc267c11c1
Adi Satria <adisatria@apps.ipb.ac.id>
2025-02-19 16:38:51 +0700

    fix: remove ssl

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), basicSsl()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
