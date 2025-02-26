38528abbe08d6db6cb8073edfe40b65ab4a23767
Adi Satria <adisatria@apps.ipb.ac.id>
2025-02-26 15:23:55 +0700

    update: enhance DatePicker and TimePicker components with onChange override and validation rules

--- src/components/forms/DatePicker.tsx
+++ src/components/forms/DatePicker.tsx
@@ -1,26 +1,47 @@
+import React from "react";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import React from 'react';
-import { Control, Controller } from 'react-hook-form';
+import { Control, Controller } from "react-hook-form";
 
-type DatePickerCtrlProps = {
-    name: string;
-    label: string;
-    control: Control<any>;
-};
-
-const DatePickerCtrl:React.FC<DatePickerCtrlProps> = ({name, label, control}) => {
-    
-    return (
-        <Controller control={control} name={name} render={({field: {onChange, value}, fieldState: {error}}) => (
-            <DatePicker
-                label={label}
-                value={value}
-                onChange={onChange}
-                slotProps={{ textField: { error: !!error, helperText: error?.message } }}
-                format='DD/MM/YYYY'
-            />
-        )}
-        />
-    )
+interface DatePickerCtrlProps {
+  name: string;
+  label: string;
+  control: Control<any>;
+  onChangeOvr?: any;
+  rules?: {
+    required: string;
+    validate?: (value: any, formValues: any) => boolean | string;
+  };
 }
+
+const DatePickerCtrl: React.FC<DatePickerCtrlProps> = ({
+  name,
+  label,
+  control,
+  rules,
+  onChangeOvr,
+}) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <DatePicker
+          label={label}
+          value={value}
+          onChange={(e) => {
+            onChange(e);
+            if (onChangeOvr !== undefined) {
+              onChangeOvr(e);
+            }
+          }}
+          slotProps={{
+            textField: { error: !!error, helperText: error?.message },
+          }}
+          format="DD/MM/YYYY"
+        />
+      )}
+    />
+  );
+};
 export default DatePickerCtrl;
