7cdf48eae58fde29440467777ccd9c37a73ba6ea
Ilham Hakim <ilhmhakim@gmail.com>
2025-02-27 09:56:02 +0700

    update: Email Template

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import theme from "./theme";
-// import Landing from "./pages/Landing";
-
 import { Refine } from "@refinedev/core";
-import { useNotificationProvider } from "@refinedev/mui";
 import dataProvider from "@refinedev/simple-rest";
-
 import { ErrorBoundary } from "react-error-boundary";
 import AdminLayout from "./components/AdminLayout";
 import { ErrorFallback } from "./error/ErrorFallback";
@@ -35,10 +31,16 @@ import { LoadingProvider } from "./providers/LoadingProvider";
 import { SnackbarProvider } from "./providers/SnackbarProvider";
 import CreateSeries from "./pages/master-data/CreateSeries";
 import {Category} from "@/pages/master-data/Category.tsx";
-import {SubTest} from "@/pages/master-data/SubTest.tsx";
 import {GroupTest} from "@/pages/master-data/group-test/GroupTest.tsx";
-import CreateEditGroupTest from "@/pages/master-data/group-test/GroupTestCreateEdit.tsx";
 import GroupTestCreateEdit from "@/pages/master-data/group-test/GroupTestCreateEdit.tsx";
+import {SubTest} from "@/pages/master-data/sub-test/SubTest.tsx";
+import {Test} from "@/pages/master-data/test/Test.tsx";
+import TestCreateEdit from "@/pages/master-data/test/TestCreateEdit.tsx";
+import SubTestCreateEdit from "@/pages/master-data/sub-test/SubTestCreateEdit.tsx";
+import SeriesDetails from "@/pages/master-data/SeriesDetails.tsx";
+import {Batch} from "@/pages/master-data/batch/Batch.tsx";
+import BatchCreateEdit from "@/pages/master-data/batch/BatchCreateEdit.tsx";
+import {EmailTemplate} from "@/pages/master-data/EmailTemplate.tsx";
 
 const WelcomeClient = lazy(() => import("@/pages/WelcomeClient"));
 const RouteProtector = lazy(() => import("@/protector/RouteProtector"));
@@ -109,6 +111,10 @@ const router = createBrowserRouter([
         element: <Series />,
       },
       {
+        path: "series/:id",
+        element: <SeriesDetails/>
+      },
+      {
         path: "series/create",
         element: <CreateSeries />,
       },
@@ -169,6 +175,26 @@ const router = createBrowserRouter([
         element: <SubTest/>
       },
       {
+        path: "subtest/create",
+        element: <SubTestCreateEdit/>
+      },
+      {
+        path: "subtest/edit/:id",
+        element: <SubTestCreateEdit/>
+      },
+      {
+        path: "test",
+        element: <Test/>
+      },
+      {
+        path: "test/create",
+        element: <TestCreateEdit/>
+      },
+      {
+        path: "test/edit/:id",
+        element: <TestCreateEdit/>
+      },
+      {
         path: "grouptest",
         element: <GroupTest/>
       },
@@ -179,7 +205,19 @@ const router = createBrowserRouter([
       {
         path: "grouptest/edit/:id",
         element: <GroupTestCreateEdit/>
+      },
+      {
+        path: "batch",
+        element: <Batch/>
+      },
+      {
+        path: "email-template",
+        element: <EmailTemplate/>
       }
+      // {
+      //   path: "batch/create",
+      //   element: <BatchCreateEdit/>
+      // }
     ],
   },
 ]);
