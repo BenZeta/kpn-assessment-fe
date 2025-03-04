fb86ac02b5e21749207ac6705bb4bf7f1be24750
Adi Satria <adisatria@apps.ipb.ac.id>
2025-03-04 13:46:37 +0700

    update: enhanced functionality for creating batch and connect it to db

--- src/components/batch/AssignmentTime.tsx
+++ src/components/batch/AssignmentTime.tsx
@@ -1,4 +1,4 @@
-import { Grid2 as Grid, Stack, Typography } from "@mui/material";
+import { Divider, Grid2 as Grid, Stack, Typography } from "@mui/material";
 import React from "react";
 import { Control } from "react-hook-form";
 import DatePickerCtrl from "../forms/DatePicker.tsx";
@@ -19,8 +19,9 @@ const AssignmentTime: React.FC<AssignmentTimeProps> = ({ control }) => {
         Assignment Time includes date and time configuration for batch
         assignment.
       </Typography>
-      <Stack spacing={2} mt={2}>
-        <Grid container spacing={2}>
+      <Divider sx={{ my: 2 }} />
+      <Stack spacing={2} mt={2} alignItems="center">
+        <Grid container spacing={2} sx={{ mt: 4 }}>
           <Grid size={{ xs: 6 }}>
             <DatePickerCtrl
               name="start_date"
@@ -29,7 +30,6 @@ const AssignmentTime: React.FC<AssignmentTimeProps> = ({ control }) => {
               rules={{ required: "Start Date is required" }}
             />
           </Grid>
-          {/* <Divider /> */}
           <Grid size={{ xs: 6 }}>
             <DatePickerCtrl
               name="end_date"
@@ -41,7 +41,7 @@ const AssignmentTime: React.FC<AssignmentTimeProps> = ({ control }) => {
                   if (!formValues.start_date || !value) return true;
                   return (
                     dayjs(value).isAfter(dayjs(formValues.start_date)) ||
-                    dayjs(value).isSame(dayjs(formValues.start_date), 'day') ||
+                    dayjs(value).isSame(dayjs(formValues.start_date), "day") ||
                     "End Date must be after Start Date"
                   );
                 },
@@ -63,27 +63,30 @@ const AssignmentTime: React.FC<AssignmentTimeProps> = ({ control }) => {
               name="end_time"
               label="End Time"
               control={control}
-              rules={{ required: "End Time is required", validate: (value, formValues) => {
-                if (
-                  !formValues.start_date ||
-                  !formValues.end_date ||
-                  !formValues.start_time ||
-                  !value
-                )
-                  return true;
-                if (
-                  dayjs(formValues.start_date).isSame(
-                    dayjs(formValues.end_date),
-                    "day"
+              rules={{
+                required: "End Time is required",
+                validate: (value, formValues) => {
+                  if (
+                    !formValues.start_date ||
+                    !formValues.end_date ||
+                    !formValues.start_time ||
+                    !value
                   )
-                ) {
-                  return (
-                    dayjs(value).isAfter(dayjs(formValues.start_time)) ||
-                    "End time must be after start time on the same day"
-                  );
-                }
-                return true;
-              }}}
+                    return true;
+                  if (
+                    dayjs(formValues.start_date).isSame(
+                      dayjs(formValues.end_date),
+                      "day"
+                    )
+                  ) {
+                    return (
+                      dayjs(value).isAfter(dayjs(formValues.start_time)) ||
+                      "End time must be after start time on the same day"
+                    );
+                  }
+                  return true;
+                },
+              }}
             />
           </Grid>
         </Grid>
--- src/components/batch/Settings.tsx
+++ src/components/batch/Settings.tsx
@@ -1,10 +1,10 @@
 import { Box, Divider, Stack, Typography } from "@mui/material";
 import React from "react";
 import { Control, Controller } from "react-hook-form";
-import { FaMicrophone } from "react-icons/fa6";
 import { FaRandom } from "react-icons/fa";
-import { RiScreenshot2Fill } from "react-icons/ri";
+import { FaMicrophone } from "react-icons/fa6";
 import { GiCardRandom } from "react-icons/gi";
+import { RiScreenshot2Fill } from "react-icons/ri";
 import CustomSwitch from "../CustomSwitch";
 
 type SettingsProps = {
@@ -58,6 +58,7 @@ const Settings: React.FC<SettingsProps> = ({ control }) => {
                   control={control}
                   render={({ field }) => (
                     <CustomSwitch
+                      value={field.value}
                       onChange={(checked) => field.onChange(checked)}
                     />
                   )}
@@ -90,6 +91,7 @@ const Settings: React.FC<SettingsProps> = ({ control }) => {
                   control={control}
                   render={({ field }) => (
                     <CustomSwitch
+                      value={field.value}
                       onChange={(checked) => field.onChange(checked)}
                     />
                   )}
