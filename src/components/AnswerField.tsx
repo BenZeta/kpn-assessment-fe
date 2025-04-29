05936d0a61e518ca19b0cd364b8739c42d3b71b2
Adi Satria <adisatria@apps.ipb.ac.id>
2025-04-29 09:41:16 +0700

    update: increase max length for answers from 5 to 7;

--- src/components/AnswerField.tsx
+++ src/components/AnswerField.tsx
@@ -29,7 +29,7 @@ const AnswerField = memo(function AnswerField({
     name: "answer",
     rules: {
       minLength: 2,
-      maxLength: 5,
+      maxLength: 7,
       validate: (): string | true =>
         validateAnswers(getValues("answer"), getValues("answer_type")),
     },
@@ -165,7 +165,7 @@ const AnswerField = memo(function AnswerField({
           </CardContent>
         </Card>
       ))}
-      {fields.length !== 5 && (
+      {fields.length !== 7 && (
         <Button
           variant="outlined"
           onClick={() =>
