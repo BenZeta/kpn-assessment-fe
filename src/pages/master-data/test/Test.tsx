d1465648fdf7c620568069cdb7d6e3a5c124518b
Adi Satria <adisatria@apps.ipb.ac.id>
2025-05-26 13:49:02 +0700

    feat: refactor Test component to use CustomTable, add loading state, and enhance action buttons with tooltips

--- src/pages/master-data/test/Test.tsx
+++ src/pages/master-data/test/Test.tsx
@@ -1,137 +1,122 @@
+import DialogComp from "@/components/Dialog.tsx";
+import { TableSkeleton } from "@/components/Skeleton.tsx";
 import useAPI from "@/hooks/useAPI.tsx";
 import useAuthStore from "@/hooks/useAuthStore.tsx";
-import { useLoading } from "@/providers/LoadingProvider.tsx";
-import useFetch from "@/hooks/useFetch.tsx";
-import { useMemo, useState } from "react";
 import useDialog from "@/hooks/useDialog.tsx";
-import { Box, Button, IconButton, Typography } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import { useNavigate } from "react-router-dom";
-import {
-  MaterialReactTable,
-  MRT_ColumnDef,
-  useMaterialReactTable,
-} from "material-react-table";
-import { TableSkeleton } from "@/components/Skeleton.tsx";
-import DialogComp from "@/components/Dialog.tsx";
+import useFetch from "@/hooks/useFetch.tsx";
+import { useLoading } from "@/providers/LoadingProvider.tsx";
 import { snack } from "@/providers/SnackbarProvider.tsx";
-import { isAxiosError } from "axios";
+import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import moment from 'moment';
+import InfoIcon from "@mui/icons-material/Info";
+import { Box, Button, IconButton, Tooltip, Typography } from "@mui/material";
+import { isAxiosError } from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import CustomTable, { CustomTableColumn } from "@/components/CustomTable";
+import moment from "moment";
+import theme from "@/theme";
 
 const Test = () => {
   const API = useAPI();
   const navigate = useNavigate();
-  const getPermission = useAuthStore((state) => state.getPermission);
+  const getPermission = useAuthStore(state => state.getPermission);
   const { showLoading, hideLoading } = useLoading();
-  const { data: test, refetch } = useFetch<{ data: any[] }>("/test");
+  const { data: test, refetch, loading } = useFetch<{ data: any[] }>("/test");
   const [selectedTest, setSelectedTest] = useState<{
     id: string;
     test_name: string;
   } | null>(null);
-  const {
-    isOpen: isOpenDelete,
-    open: openDelete,
-    close: closeDelete,
-  } = useDialog();
+  const { isOpen: isOpenDelete, open: openDelete, close: closeDelete } = useDialog();
 
-  const columns: MRT_ColumnDef<any>[] = useMemo(
-    () => [
-      {
-        header: "Name",
-        accessorKey: "test_name",
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center" },
+  const columns: CustomTableColumn<any>[] = [
+    {
+      header: "Name",
+      accessorKey: "test_name",
+      muiTableHeadCellProps: { align: "center" },
+      muiTableBodyCellProps: { align: "center" },
+    },
+    {
+      header: "Code",
+      accessorKey: "test_code",
+      muiTableHeadCellProps: { align: "center" },
+      muiTableBodyCellProps: { align: "center" },
+    },
+    {
+      header: "Total Sub Test",
+      accessorKey: "subtest_count",
+      muiTableHeadCellProps: { align: "center" },
+      muiTableBodyCellProps: { align: "center" },
+    },
+    {
+      header: "Status",
+      accessorKey: "is_active",
+      muiTableHeadCellProps: { align: "center" },
+      muiTableBodyCellProps: { align: "center" },
+      Cell: ({ cell }: any) => (cell.getValue() ? "Active" : "Inactive"),
+    },
+    {
+      header: "Created By",
+      accessorKey: "created_by",
+      muiTableHeadCellProps: { align: "center" },
+      muiTableBodyCellProps: { align: "center" },
+    },
+    {
+      header: "Created At",
+      accessorKey: "created_at",
+      muiTableHeadCellProps: { align: "center" },
+      muiTableBodyCellProps: { align: "center" },
+      Cell: ({ cell }) => {
+        const value = cell.getValue();
+        return value ? moment(value).format("MMMM DD, YYYY hh:mm A") : "";
       },
-      {
-        header: "Code",
-        accessorKey: "test_code",
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center" },
-      },
-      {
-        header: "Total Sub Test",
-        accessorKey: "subtest_count",
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center" },
-      },
-      {
-        header: "Status",
-        accessorKey: "is_active",
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center" },
-        Cell: ({ cell }: any) => (cell.getValue() ? "Active" : "Inactive"),
-      },
-      {
-        header: "Created By",
-        accessorKey: "created_by",
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center" },
-      },
-      {
-        header: "Created At",
-        accessorKey: "created_at",
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center" },
-        Cell: ({ cell }) => {
-          const value = cell.getValue();
-          return value ? moment(value).format("MMMM DD, YYYY hh:mm A") : '';
-        }
-      },
-      {
-        header: "Actions",
-        accessorKey: "actions",
-        enableSorting: false,
-        enableColumnFilter: false,
-        muiTableHeadCellProps: { align: "center" },
-        muiTableBodyCellProps: { align: "center" },
-        Cell: ({ row }) => {
-          const id = row.original.id;
-          const test_name = row.original.test_name;
-          return (
-            <Box
-              sx={{ display: "flex", justifyContent: "flex-end", gap: "8px" }}
-            >
+    },
+    {
+      header: "Actions",
+      accessorKey: "actions",
+      enableSorting: false,
+      enableColumnFilter: false,
+      muiTableHeadCellProps: { align: "center" },
+      muiTableBodyCellProps: { align: "center" },
+      Cell: ({ row }) => {
+        const id = row.original.id;
+        const test_name = row.original.test_name;
+        return (
+          <Box sx={{ display: "flex", justifyContent: "flex-end", gap: "8px" }}>
+            {getPermission("fupdate", 5) && (
+              <Tooltip title="Edit Test" placement="top" arrow>
+                <IconButton
+                  onClick={() => navigate(`/admin/test/edit/${id}`)}
+                  aria-label="edit"
+                  size="small"
+                >
+                  <EditIcon sx={{ color: "secondary.dark" }} />
+                </IconButton>
+              </Tooltip>
+            )}
+            <Tooltip title="View Details" placement="top" arrow>
               <IconButton
                 onClick={() => navigate(`/admin/test/detail/${id}`)}
                 aria-label="edit"
                 size="small"
               >
-                <VisibilityIcon />
+                <InfoIcon sx={{ color: "info.light" }} />
               </IconButton>
-              <IconButton
-                onClick={() => navigate(`/admin/test/edit/${id}`)}
-                aria-label="edit"
-                size="small"
-              >
-                <EditIcon />
-              </IconButton>
-              <IconButton
-                color="error"
-                onClick={() => handleOpenDelete(id, test_name)}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </Box>
-          );
-        },
+            </Tooltip>
+            {getPermission("fdelete", 6) && (
+              <Tooltip title="Delete Test" placement="top" arrow>
+                <IconButton color="primary" onClick={() => handleOpenDelete(id, test_name)}>
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            )}
+          </Box>
+        );
       },
-    ],
-    []
-  );
-
-  const table = useMaterialReactTable({
-    columns,
-    data: test?.data ?? [],
-    getRowId: (row) => row.id,
-    enablePagination: true,
-    enableColumnFilters: true,
-    enableSorting: true,
-    enableRowSelection: false,
-    enableRowActions: false, // Matikan renderRowActions karena sudah ada di columns
-  });
+    },
+  ];
 
   const handleOpenDelete = (id: string, test_name: string) => {
     setSelectedTest({ id, test_name });
@@ -158,27 +143,43 @@ const Test = () => {
   };
 
   return (
-    <Box sx={{ p: 3 }}>
-      <Box sx={{ mb: 2 }}>
-        <Typography variant="h2" component="div">
+    <Box
+      sx={{
+        p: 3,
+        height: "100%",
+        bgcolor: theme.palette.background.paper,
+        borderRadius: 2,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box sx={{ mb: 2, display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <Typography variant="h1" color="primary">
           Test
-          {getPermission("fcreate", 14) && (
-            <Button
-              startIcon={<AddIcon />}
-              variant="contained"
-              onClick={() => navigate(`/admin/test/create`)}
-              sx={{ ml: 2 }}
-            >
-              Create Test
-            </Button>
-          )}
         </Typography>
+        {getPermission("fcreate", 14) && (
+          <Button
+            startIcon={<AddIcon />}
+            variant="contained"
+            onClick={() => navigate(`/admin/test/create`)}
+            sx={{ ml: 2 }}
+          >
+            Create Test
+          </Button>
+        )}
       </Box>
 
-      {test?.data?.length ? (
-        getPermission("fread", 14) && <MaterialReactTable table={table} />
-      ) : (
+      {loading ? (
         <TableSkeleton column={4} row={2} small />
+      ) : (
+        <Box sx={{ flex: 1, minWidth: 0, overflow: "auto" }}>
+          <CustomTable
+            columns={columns}
+            data={test?.data || []}
+            hasPermission={getPermission("fread", 14)}
+            enableFilters={true}
+          />
+        </Box>
       )}
 
       <DialogComp
