160ffdd3e8acfc68162195f319e7224e21d55f8c
Adi Satria <adisatria@apps.ipb.ac.id>
2025-06-17 14:38:37 +0700

    feat: add CoverPage component for report cover selection

---
+++ src/components/report/CoverPage.tsx
@@ -0,0 +1,50 @@
+import { Typography, Box } from "@mui/material";
+import React from "react";
+import CaraouselCardCover from "./CaraouselCardCover";
+import CardCoverField from "./CardCoverField";
+
+type CoverPageProps = {
+  control?: any;
+};
+
+const CoverPage: React.FC<CoverPageProps> = ({ control }) => {
+  return (
+    <>
+      <Typography variant="h6" color="textSecondary" fontWeight={600} sx={{ mb: 2 }}>
+        Cover
+      </Typography>
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <Box
+          sx={theme => ({
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+            p: 2,
+            backgroundColor: theme.palette.grey[400],
+          })}
+        >
+          <Typography>Chosen</Typography>
+
+          <CardCoverField name="cover_id" control={control} />
+        </Box>
+
+        <Box
+          sx={{
+            height: "26rem",
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+            flexGrow: 1,
+            py: 1,
+          }}
+        >
+          <CaraouselCardCover />
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <em>Choose Report Cover</em>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+export default CoverPage;
