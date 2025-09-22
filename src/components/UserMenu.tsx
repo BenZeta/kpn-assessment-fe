51b0278d5cc0d8f8e23403005b6277611eea4ed3
tectanagrand <rtektano@gmail.com>
2025-09-22 15:51:02 +0700

    fix : bug at user menu

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -54,17 +54,17 @@ const UserMenu = () => {
             }}
           >
             <IconButton>
-              <Avatar>{fullname.slice(0, 1).toUpperCase()}</Avatar>
+              <Avatar>{fullname && fullname.slice(0, 1).toUpperCase()}</Avatar>
             </IconButton>
-            <Typography sx={{ textWrap: "wrap" }}>{fullname}</Typography>
+            <Typography sx={{ textWrap: "wrap" }}>{fullname ?? ""}</Typography>
           </Box>
         </MenuItem>
         <MenuItem onClick={handleLogout} sx={{ width: "10rem" }}>
           Logout
         </MenuItem>
-        <MenuItem onClick={handleLogout} sx={{ width: "10rem" }}>
+        {/* <MenuItem onClick={handleLogout} sx={{ width: "10rem" }}>
           <QuestionAnswerOutlined /> User Guide
-        </MenuItem>
+        </MenuItem> */}
       </Menu>
     </>
   );
